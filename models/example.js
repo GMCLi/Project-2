fd18cc5838a7b9e16bd9bdbe2f117fc536a83450
Gordon Li <li.mc.gordon@gmail.com>
2019-03-14 10:25:55 -0400

    customer creation issues

--- models/example.js
+++ models/example.js
@@ -1,14 +1,9 @@
 module.exports = function(sequelize, DataTypes) {
   var customer = sequelize.define("customer", {
     customerName: DataTypes.STRING,
-<<<<<<< HEAD
-    customerDateOfBirth: DataTypes.INTEGER,
-    customerTelephone: DataTypes.INTEGER
-=======
     customerDOB: DataTypes.DATEONLY,
     customerNum: DataTypes.INTEGER
     // customerRenting: DataTypes.BOOLEAN
->>>>>>> 4719602173955926178bd7b040039d96ea0c8391
   });
   return customer;
 };
